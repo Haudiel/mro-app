b2a47e6385ec86362a1c27dc04a546f547b4afae
Haudielx <l19330632@hermosillo.tecnm.mx>
2023-08-04 13:52:20 -0700

    Agregar tablas

--- src/app/components/page-two/two-component.ts
+++ src/app/components/page-two/two-component.ts
@@ -1,13 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+
+
+interface EditingStatus {
+  [key: string]: boolean;
+}
+
+interface RowObject {
+  [key: string]: string | number; // Assuming the properties can be strings or numbers
+}
 
 @Component({
   selector: 'one-component',
   templateUrl: './two-component.html',
   styleUrls: ['./two-component.css'],
+
 })
 
 export class TwoPageComponent implements OnInit {
+
+  editing: EditingStatus = {};
+  rows: RowObject[] = [];
+
+  ColumnMode = ColumnMode;
+
+  constructor() {
+    this.fetch((data: never[]) => {
+      this.rows = data;
+    });
+  }
+
+  fetch(cb: { (data: never[]): void; (arg0: any): void; }) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/company.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  updateValue(event: any, cell: string, rowIndex: number) {
+    console.log('inline editing rowIndex', rowIndex);
+    this.editing[rowIndex + '-' + cell] = false;
+    this.rows[rowIndex][cell] = event.target.value;
+    this.rows = [...this.rows];
+    console.log('UPDATED!', this.rows[rowIndex][cell]);
+  }
 
   mroForm = new UntypedFormGroup({
     mroFrm: new UntypedFormControl(''),
@@ -17,5 +58,5 @@ export class TwoPageComponent implements OnInit {
 
   }
 
-  onSubmit(){}
+  onSubmit() { }
 }
--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -41,6 +41,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 @NgModule({
   exports: [
     A11yModule,
@@ -86,6 +88,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     PortalModule,
     ScrollingModule,
     MatFormFieldModule,
+    NgxDatatableModule,
   ]
 })
 export class MaterialModule {}
